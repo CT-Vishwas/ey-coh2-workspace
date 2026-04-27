a933ede9875b5c0c1d1e822f2a63e896bfa647d0
CT-Vishwas <vishwas@cloudthat.com>
2026-04-27 07:37:26 +0000

    Add initial Python scripts and interactive notebook for demonstration

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Hello World")
+my_name = "vishwas"
+my_age = 10
+price = 23.45
+print("The Values of my_name is: ", my_name, "its type is: ", type(my_name))
+print("The Values of my_age is: ", my_age, "its type is: ", type(my_age))
+print("The Values of price is: ", price, "its type is: ", type(price))
---
+++ si_calculator.py
@@ -0,0 +1,14 @@
+# This is a comment, It is not processed by interpreter
+# Author: Vishwas K Singh
+# Email: vishwas@cloudthat.com
+# Script to calculate the simple interest
+# si = ptr / 100
+
+
+principal = 300000
+time_in_months =36
+rate_of_interest = 0.07
+
+simple_interest = (principal * time_in_months * rate_of_interest) / 100
+
+print(simple_interest)
