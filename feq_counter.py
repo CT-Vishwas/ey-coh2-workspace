a0394ae423a2d00ac6b49c3a3715841697db6ef7
Vishwas K Singh <vishwas@cloudthat.com>
2026-04-28 16:35:32 +0530

    frequency counter1

--- feq_counter.py
+++ feq_counter.py
@@ -2,9 +2,16 @@
 
 inp_str = input("Enter the String: ")
 inp_str = inp_str.lower()
+# vishwasis
 
 freq_counts = {}
+# for chr in inp_str:
+#     freq_counts[chr] = freq_counts.get(chr,0) + 1
+
 for chr in inp_str:
-    freq_counts[chr] = freq_counts.get(chr,0) + 1
+    if chr not in freq_counts.keys():
+        freq_counts[chr] = 0+1
+    else:
+        freq_counts[chr] = freq_counts[chr] + 1
 
 print(f"Character Frequencies: {freq_counts}")
